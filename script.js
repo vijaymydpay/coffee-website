c9900c0b36beec9bcb57411acf809a0d37524143
vijaymydpay <140145228+vijaymydpay@users.noreply.github.com>
2023-09-15 16:54:52 +0530

    added

---
+++ script.js
@@ -0,0 +1,47 @@
+function gotoLink(link){
+    console(link.value);
+    window.open(link.value);
+}
+
+function gotoLink(link){
+    console.log(link.value);
+    window.open(link.value);
+}
+
+function gotoLink(link){
+    console.log(link.value);
+    // location.href = link.value;
+    window.open(link.value);
+}
+
+function gotoLink(link){
+     console.log(link.value);
+     window.open(link.value);
+}
+
+function gotoLink(link){
+    console.log(link.value);
+    window.open(link.value);
+}
+
+function gotoLink(link){
+    console.log(link.value);
+    window.open(link.value)
+}
+
+// --------------See More---------------//
+
+function gotoLink(link){
+    console.log(link.value);
+    window.open(link.value);
+}
+
+// ----------------see more end----------//
+
+
+// -------------------about--------------//
+
+function gotoLink(link){
+    console.log(link.value);
+    window.open(link.value);
+}
